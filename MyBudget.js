512c01ce2cb08ebdc37f9198c11b551b9363b149
SivanPesahov <sivanp27540@gmail.com>
2024-05-09 11:55:14 +0300

    09/05/24

--- MyBudget.js
+++ MyBudget.js
@@ -1,5 +1,5 @@
-let incomeArr
-let expensesArr
+let incomeArr = []
+let expensesArr = []
 let totalIncome
 let totalExpenses
 
@@ -11,3 +11,42 @@ let incomeArrKey
 let expensesArrKey
 
 //------------------------------------------------
+
+
+function getInputs(){
+    let elemDescriptionInput = document.querySelector("#descriptionID")
+    let elemValueInput = document.querySelector("#valueID")
+    let newObj = {description : elemDescriptionInput.value, amount : elemValueInput.value}
+    return newObj
+}
+
+function addToArr(obj){
+    elemToggle = document.querySelector("#toggle")
+    if(elemToggle.value == 'plus'){
+        incomeArr.push(obj)
+        totalIncome += obj.amount
+    }
+    else{
+        expensesArr.push(obj)
+        totalExpenses += obj.amount
+    }
+}
+
+function updateBalance(totalIncome, totalExpenses){
+    return totalIncome - totalExpenses
+}
+
+function saveToLocalStorage(key, arr){
+    localStorage.setItem(key, JSON.stringify(arr))
+}
+
+function submit(){
+    let obj = getInputs()
+    // if (validateInput(obj)){
+        addToArr(obj)
+        updateBalance(totalIncome, totalExpenses)
+        //addRow()
+        saveToLocalStorage("incomeArrKey", incomeArr)
+        saveToLocalStorage("expensesArrKey", expensesArr)
+    // }
+}
