d62d0d523380303665905a136e80a7f308b6c454
leosilberg <ariel.silberg@gmail.com>
2024-05-09 12:00:04 +0300

    listContainer  and init funcs

--- MyBudget.js
+++ MyBudget.js
@@ -1,13 +1,47 @@
-let incomeArr
-let expensesArr
-let totalIncome
-let totalExpenses
+let incomeArrKey;
+let expensesArrKey;
 
-let elemToggle
-let elemIncomeList
-let elemExpensesList
+let incomeArr = readFromLocalStorage(incomeArrKey);
+let expensesArr = readFromLocalStorage(expensesArrKey);
+let totalIncome = calculateTotal(incomeArr);
+let totalExpenses = calculateTotal(expensesArr);
 
-let incomeArrKey
-let expensesArrKey
+let elemToggle;
+let elemIncomeList = document.querySelector("#incomeList");
+let elemExpensesList = document.querySelector("#expensesList");
+
+let elemDate;
 
 //------------------------------------------------
+
+function readFromLocalStorage(key) {
+  let temp = localStorage.getItem(key);
+  return temp !== null ? temp : [{ description: "TEST", amount: 3000 },{ description: "TEST2", amount: 3000 }];
+}
+
+function calculateTotal(arr) {
+  return arr.reduce((sum, val) => {
+    sum += val.amount;
+  });
+}
+
+function addRow(obj, elemList) {
+  let symbol = elemList == elemIncomeList ? "+" : "-";
+  elemList.innerHTML += `<li><p style="color:black;">${obj.description}</p><div><p>${symbol} ${obj.amount}</p><i class="fa-regular fa-circle-xmark" onclick="deleteRow(this)"></i></div></li>`;
+}
+
+function displayRowsFromLocalStorage(arr, elemList) {
+  arr.forEach((obj) => {
+    addRow(obj, elemList);
+  });
+}
+
+function displayDate() {
+  elemDate.innerText = `Available Budget in ${new Date().toLocaleString(
+    "default",
+    { month: "long", year: "numeric" }
+  )}`;
+}
+
+displayRowsFromLocalStorage(incomeArr, elemIncomeList);
+displayRowsFromLocalStorage(expensesArr, elemExpensesList);
