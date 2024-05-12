5e521ca841a6d29423629efdd45e3cf3ac958422
leosilberg <ariel.silberg@gmail.com>
2024-05-12 14:12:48 +0300

    Updates

--- MyBudget.js
+++ MyBudget.js
@@ -6,105 +6,77 @@ let expensesArr = readFromLocalStorage(expensesArrKey);
 let totalIncome = calculateTotal(incomeArr);
 let totalExpenses = calculateTotal(expensesArr);
 
-
 const elemIncomeList = document.querySelector("#incomeList");
 const elemExpensesList = document.querySelector("#expensesList");
-let elemInput = document.querySelector('#descriptionID')
-let elemValue = document.querySelector('#valueID')
-let elemToggle = document.querySelector("#toggle")
-let elemV = document.querySelector('#vID')
+const elemDescriptionInput = document.querySelector("#descriptionInput");
+const elemAmountInput = document.querySelector("#amountInput");
+const elemTypeToggle = document.querySelector("#typeToggle");
+const elemSubmitButton = document.querySelector("#submitButton");
 
-const elemDate = document.querySelector("#timeArea");
+const elemDate = document.querySelector("#date");
 const elemBalance = document.querySelector("#totalBalance");
 const elemIncomeTotal = document.querySelector("#incomeTotal");
 const elemExpensesTotal = document.querySelector("#expensesTotal");
-const elemPercentage = document.querySelector("#percentageId"); 
+const elemTotalPercentage = document.querySelector("#totalPercentage");
 
 //------------------------------------------------
 
-
-function getInputs(){
-    let elemDescriptionInput = document.querySelector("#descriptionID")
-    let elemValueInput = document.querySelector("#valueID")
-    let newObj = {description : elemDescriptionInput.value, amount : parseFloat(elemValueInput.value)}
-    return newObj
+function getInputs() {
+  let transaction = {
+    description: elemDescriptionInput.value,
+    amount: parseFloat(elemAmountInput.value),
+  };
+  return transaction;
 }
 
-function addToArr(obj){
-    if(elemToggle.value == 'plus'){
-        incomeArr.push(obj)
-        totalIncome += obj.amount
-        addRow(obj, elemIncomeList)
-    }
-    else{
-        expensesArr.push(obj)
-        totalExpenses += obj.amount
-        addRow(obj, elemExpensesList)
-
-    }
+function validateInput(transaction) {
+  if (
+    isFinite(transaction.amount) &&
+    transaction.description != null &&
+    transaction.description.trim() !== ""
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function toggleClicked(element){
-    if(element.value == 'plus'){
-        changeColorV('green', elemV)
-    }
-    else{
-        changeColorV('red', elemV)
-    }
+function addTransaction(transaction) {
+  if (elemTypeToggle.value == "income") {
+    incomeArr.push(transaction);
+    saveToLocalStorage(incomeArrKey, incomeArr);
+    totalIncome += transaction.amount;
+    addRow(transaction, elemIncomeList);
+  } else {
+    expensesArr.push(transaction);
+    saveToLocalStorage(expensesArrKey, expensesArr);
+    totalExpenses += transaction.amount;
+  }
 }
 
-function changeColorInput(color, element){
-    
-    element.style.borderColor = color;
+function submit() {
+  let transaction = getInputs();
+  if (validateInput(transaction)) {
+    addTransaction(transaction);
+    displayTotals();
+    displayBalance();
+    displayTransactions(expensesArr, elemExpensesList);
+    clearInputs();
+  }
 }
 
-function changeColorV(color, element){
-    
-    element.style.color = color;
-}
-
-function updateBalance(totalIncome, totalExpenses){
-    return totalIncome - totalExpenses
+function clearInputs() {
+  elemDescriptionInput.value = "";
+  elemAmountInput.value = "";
 }
 
 function saveToLocalStorage(key, arr) {
   localStorage.setItem(key, JSON.stringify(arr));
 }
 
-function submit() {
-  let obj = getInputs();
-  if (validateInput(obj)) {
-    addToArr(obj);
-    displayTotals();
-    displayBalance();
-    displayRows(expensesArr, elemExpensesList);
-
-    saveToLocalStorage(incomeArrKey, incomeArr);
-    saveToLocalStorage(expensesArrKey, expensesArr);
-  }
-}
-
-function validateInput(obj) {
-  console.log(obj);
-  if (
-    isFinite(obj.amount) &&
-    obj.description != null &&
-    obj.description.trim() !== ""
-  ) {
-    return true;
-  } else {
-    return false;
-    }
-}
-
 function readFromLocalStorage(key) {
   let temp = localStorage.getItem(key);
-  return temp !== null
-    ? JSON.parse(temp)
-    : [
-        { description: "TEST", amount: 3000 },
-        { description: "TEST2", amount: 3000 },
-      ];
+  return temp !== null ? JSON.parse(temp) : [];
 }
 
 function calculateTotal(arr) {
@@ -113,53 +85,51 @@ function calculateTotal(arr) {
   }, 0);
 }
 
-function addRow(obj, elemList) {
-  let symbol = elemList == elemIncomeList ? "+" : "-";
-  elemList.innerHTML += `<li><p style="color:black;">${
-    obj.description
-  }</p><div><p class="item_amount">${symbol}${obj.amount.toFixed(
-    2
-  )}</p>
-  
-  ${symbol == '-' ? `<div class = 'percentage'> <p>${parseFloat((obj.amount / totalIncome)*100).toFixed(0)}%</p></div>`:""}
-  <i class="fa-regular fa-circle-xmark" onclick="deleteRow(this)"></i></div></li>`; 
-
+function addRow(transaction, elemList) {
+  const symbol = elemList == elemIncomeList ? "+" : "-";
+  elemList.innerHTML += `<li class="transaction__item">
+  <p class="transaction__description">${transaction.description}</p>
+  <div class="transaction__details">
+  <p class="transaction__amount">${symbol}${transaction.amount.toFixed(2)}</p>
+  ${
+    symbol == "-"
+      ? `<div class = 'percentage__container'>
+       <p>${parseFloat((transaction.amount / totalIncome) * 100).toFixed(
+         0
+       )}%</p>
+       </div>`
+      : ""
+  }
+  <i class="fa-regular fa-circle-xmark" onclick="deleteTransaction(this)"></i></div></li>`;
 }
 
-function createList(){
-  
+function indexOfTransaction(element, elemList) {
+  return Array.from(elemList.children).indexOf(element.parentNode.parentNode);
 }
 
-function deleteRow(element) {
+function deleteTransaction(element) {
   let amount = parseFloat(
-    element.parentNode.querySelector(".item_amount").innerText
+    element.parentNode.querySelector(".transaction__amount").innerText
   );
   if (amount > 0) {
-    incomeArr.splice(
-      Array.from(elemIncomeList.children).indexOf(
-        element.parentNode.parentNode
-      ),
-      1
-    );
+    incomeArr.splice(indexOfTransaction(element, elemIncomeList), 1);
+    saveToLocalStorage(incomeArrKey, incomeArr);
     totalIncome -= amount;
   } else {
-    expensesArr.splice(
-      Array.from(elemExpensesList.children).indexOf(
-        element.parentNode.parentNode
-      ),
-      1
-    );
+    expensesArr.splice(indexOfTransaction(element, elemExpensesList), 1);
+    saveToLocalStorage(expensesArrKey, expensesArr);
     totalExpenses += amount;
   }
   element.parentNode.parentNode.remove();
   displayTotals();
   displayBalance();
+  displayTransactions(expensesArr, elemExpensesList);
 }
 
-function displayRows(arr, elemList) {
-  elemList.innerHTML = ''
-  arr.forEach((obj) => {
-    addRow(obj, elemList);
+function displayTransactions(arr, elemList) {
+  elemList.innerHTML = "";
+  arr.forEach((transaction) => {
+    addRow(transaction, elemList);
   });
 }
 
@@ -167,48 +137,63 @@ function displayDate() {
   elemDate.innerText = `Available Budget in ${new Date().toLocaleString(
     "default",
     { month: "long", year: "numeric" }
-  )}`;
+  )}:`;
 }
 
 function displayTotals() {
-  elemIncomeTotal.innerText = totalIncome.toFixed(2);
-  elemExpensesTotal.innerText = totalExpenses.toFixed(2);
-  elemPercentage.innerText = `${((totalExpenses/totalIncome)*100).toFixed(0)}%`
-
+  elemIncomeTotal.innerText = `+ ${totalIncome.toFixed(2)}`;
+  elemExpensesTotal.innerText = `- ${totalExpenses.toFixed(2)}`;
+  elemTotalPercentage.innerText = `${(
+    (totalExpenses / totalIncome) *
+    100
+  ).toFixed(0)}%`;
 }
 
 function displayBalance() {
-  elemBalance.innerText = (totalIncome - totalExpenses).toFixed(2);
+  const balance = (totalIncome - totalExpenses).toFixed(2);
+  elemBalance.innerText = balance > 0 ? `+${balance}` : balance;
 }
 
-function addEvent(element){
-    element.addEventListener('keypress', function(event) {
-        if (event.key === 'Enter') {
-            submit()
-          }
-    })
-    element.addEventListener('focus', function() {
-        if(elemToggle.value == 'plus'){
-            changeColorInput('green', element)
-        }
-        else{
-            changeColorInput('red', element)
-        }
-    })
-    element.addEventListener('blur', function() {
-        if(elemToggle.value == 'plus'){
-            changeColorInput('', element)
-        }
-        else{
-            changeColorInput('', element)
-        }
-    })
+function addEvent(element) {
+  element.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      submit();
+    }
+  });
+  element.addEventListener("focus", function () {
+    if (elemTypeToggle.value == "income") {
+      setBorderColor("var(--clr-income)", element);
+    } else {
+      setBorderColor("var(--clr-expenses)", element);
+    }
+  });
+  element.addEventListener("blur", function () {
+    setBorderColor("", element);
+  });
 }
 
-addEvent(elemInput)
-addEvent(elemValue)
-displayRows(incomeArr, elemIncomeList);
-displayRows(expensesArr, elemExpensesList);
+function setBorderColor(color, element) {
+  element.style.borderColor = color;
+}
+
+function toggleClicked(element) {
+  if (element.value == "income") {
+    setButtonColor("var(--clr-income)", elemSubmitButton);
+    setBorderColor("var(--clr-income)", elemTypeToggle);
+  } else {
+    setButtonColor("var(--clr-expenses)", elemSubmitButton);
+    setBorderColor("var(--clr-expenses)", elemTypeToggle);
+  }
+}
+
+function setButtonColor(color, element) {
+  element.style.color = color;
+}
+
+addEvent(elemDescriptionInput);
+addEvent(elemAmountInput);
+displayTransactions(incomeArr, elemIncomeList);
+displayTransactions(expensesArr, elemExpensesList);
 displayDate();
 displayTotals();
 displayBalance();
